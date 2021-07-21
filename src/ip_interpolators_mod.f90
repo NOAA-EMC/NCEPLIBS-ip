43178934c3d75bafc36cb46c74e9167bc5a31eec
kgerheiser <kgerheiser@icloud.com>
2021-07-20 21:52:16 -0400

    Add Doxygen docs to ip_interpolators_mod

--- src/ip_interpolators_mod.f90
+++ src/ip_interpolators_mod.f90
@@ -1,19 +1,34 @@
-module ip_interpolators_mod
-  ! re-export specific interpolation routines
-  ! use bilinear_interpolator_scalar_mod
-  ! use bicubic_interpolator_scalar_mod
-  ! use neighbor_interpolator_scalar_mod
-  ! use budget_interpolator_scalar_mod
-  ! use spectral_interpolator_scalar_mod
-  ! use neighbor_budget_interpolator_scalar_mod
+!> @file
+!! @brief Top-level module to export interpolation routines and constants.
+!! @author Kyle Gerheiser
 
+!> Top-level module to export interpolation routines and constants.
+!! @author Kyle Gerheiser
+module ip_interpolators_mod
   implicit none
 
+  use bilinear_interp_mod
+  use bicubic_interp_mod
+  use budget_interp_mod
+  use neighbor_interp_mod
+  use spectral_interp_mod
+  use neighbor_budget_interp_mod
+
+  !> @param Constant to choose BILINEAR interpolation method
   integer, parameter, public :: BILINEAR_INTERP_ID = 0
+  !> @param Constant to choose BICUBIC interpolation method
   integer, parameter, public :: BICUBIC_INTERP_ID = 1
+  !> @param Constant to choose NEIGBOR interpolation method
   integer, parameter, public :: NEIGHBOR_INTERP_ID = 2
+  !> @param Constant to choose BUDGET interpolation method
   integer, parameter, public :: BUDGET_INTERP_ID = 3
+  !> @param Constant to choose SPECTRAL interpolation method
   integer, parameter, public :: SPECTRAL_INTERP_ID = 4
+  !> @param Constant to choose NEIGBOR_BUDGET interpolation method
   integer, parameter, public :: NEIGHBOR_BUDGET_INTERP_ID = 6
+
+contains
+
+
 end module ip_interpolators_mod
 
