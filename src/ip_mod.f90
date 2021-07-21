9fc99f0d18271d140bbff848a8be2aa27163e0a1
kgerheiser <kgerheiser@icloud.com>
2021-07-20 21:52:59 -0400

    Make interpolation ids public

--- src/ip_mod.f90
+++ src/ip_mod.f90
@@ -4,6 +4,16 @@
 
 !> Top-level module for the ip library which re-exports public routines such as ipolates, ipolatev, and gdswzd.
 module ip_mod
+
+  ! Make these constants public to everyone instead of
+  ! using numbers directly
+  use ip_interpolators_mod, only: BILINEAR_INTERP_ID, &
+       BICUBIC_INTERP_ID, &
+       NEIGHBOR_INTERP_ID, &
+       BUDGET_INTERP_ID, &
+       SPECTRAL_INTERP_ID, &
+       NEIGHBOR_BUDGET_INTERP_ID
+       
   use ipolates_mod
   use ipolatev_mod
   use gdswzd_mod
