00d8f61d8df7d2c6b472979d6060e3f63df0d044
kgerheiser <kgerheiser@icloud.com>
2021-07-27 17:21:33 -0400

    Add Doxygen to ip_grid_mod

--- src/ip_grid_mod.f90
+++ src/ip_grid_mod.f90
@@ -1,25 +1,57 @@
+!> @file
+!! @brief Abstract ip_grid type
+!!
+!! @author Kyle Gerheiser
+!! @date July 2021
+
+!> Abstract ip_grid type
 module ip_grid_mod
   use ip_grid_descriptor_mod
   implicit none
 
+  !> @param Integer grid number for equidistant cylindrical grid in grib1
   integer, public, parameter :: EQUID_CYLIND_GRID_ID_GRIB1 = 0
+  !> @param Integer grid number for Mercator grid in grib1
   integer, public, parameter :: MERCATOR_GRID_ID_GRIB1 = 1
+  !> @param Integer grid number for Lambert Conformal grid in grib1
   integer, public, parameter :: LAMBERT_CONF_GRID_ID_GRIB1 = 3
+  !> @param Integer grid number for Gaussian grid in grib1
   integer, public, parameter :: GAUSSIAN_GRID_ID_GRIB1 = 4
+  !> @param Integer grid number for polar stereo grid in grib1
   integer, public, parameter :: POLAR_STEREO_GRID_ID_GRIB1 = 5
+  !> @param Integer grid number for rotated equidistant cylindrical E-stagger grid
   integer, public, parameter :: ROT_EQUID_CYLIND_E_GRID_ID_GRIB1 = 203
+  !> @param Integer grid number for rotated equidistant cylindrical B-stagger grid
   integer, public, parameter :: ROT_EQUID_CYLIND_B_GRID_ID_GRIB1 = 205
 
+  !> @param Integer grid number for equidistant cylindrical grid in grib2
   integer, public, parameter :: EQUID_CYLIND_GRID_ID_GRIB2 = 0
+  !> @param Integer grid number for rotated equidistant cylindrical grid in grib2
   integer, public, parameter :: ROT_EQUID_CYLIND_GRID_ID_GRIB2 = 1
+  !> @param Integer grid number for Mercator grid in grib2
   integer, public, parameter :: MERCATOR_GRID_ID_GRIB2 = 10
+  !> @param Integer grid number for polar stereo grid in grib2
   integer, public, parameter :: POLAR_STEREO_GRID_ID_GRIB2 = 20
+  !> @param Integer grid number for Lambert conformal grid in grib2
   integer, public, parameter :: LAMBERT_CONF_GRID_ID_GRIB2 = 30
+  !> @param Integer grid number for Gaussian grid in grib2
   integer, public, parameter :: GAUSSIAN_GRID_ID_GRIB2 = 40
 
   private
   public :: ip_grid, gdswzd_interface, operator(==)
 
+  !> Abstract grid that holds fields and methods common to all grids.
+  !! ip_grid is meant to be subclassed when implementing a new grid.
+  !!
+  !! There are three methods that must be implemented: init_grib1, init_grib2, and gdswzd.
+  !!
+  !! The init methods are responsible for setting up the grid
+  !! using grib1/grib2 descriptors.
+  !!
+  !! Gdswzd performs transformations to/from Earth coordinates and grid coordinates.
+  !!
+  !! @author Kyle Gerheiser
+  !! @date July 2021
   type, abstract :: ip_grid
      class(ip_grid_descriptor), allocatable :: descriptor
 
@@ -73,11 +105,30 @@ module ip_grid_mod
 
 contains
 
+  !> Compares two grids.
+  !!
+  !! @param[in] grid1 An ip_grid
+  !! @param[in] grid2 Another ip_grid
+  !!
+  !! @return True if the grids are the same, false if not.
+  !!
+  !! @author Kyle Gerheiser
+  !! @date July 2021
   logical function is_same_grid(grid1, grid2)
     class(ip_grid), intent(in) :: grid1, grid2
     is_same_grid = grid1%descriptor == grid2%descriptor
   end function is_same_grid
-  
+
+  !> Returns the field position for a given grid point.
+  !!
+  !! @param[in] self
+  !! @param[in] i
+  !! @param[in] j
+  !!
+  !! @return Integer position in grib field to locate grid point.
+  !!
+  !! @author Kyle Gerheiser
+  !! @date July 2021
   function field_pos(self, i, j)
     class(ip_grid), intent(in) :: self
     integer, intent(in) :: i, j
