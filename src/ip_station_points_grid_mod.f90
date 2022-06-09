447b0480bf3dcc4072e12bfcf6177e48c9294831
Edward Hartnett <Edward.Hartnett@noaa.gov>
2022-06-09 07:27:23 +0300

    more doxygen

--- src/ip_station_points_grid_mod.f90
+++ src/ip_station_points_grid_mod.f90
@@ -1,3 +1,11 @@
+!> @file
+!> @brief ???
+!>
+!> @author Iredell @date 96-04-10
+
+!> @brief ???
+!>
+!> @author Iredell @date 96-04-10
 module ip_station_points_grid_mod
   use ip_grid_descriptor_mod
   use ip_grid_mod
@@ -9,26 +17,56 @@ module ip_station_points_grid_mod
 
   type, extends(ip_grid) :: ip_station_points_grid
    contains
-     procedure :: init_grib1
-     procedure :: init_grib2
-     procedure :: gdswzd => GDSWZD_station_points
+     procedure :: init_grib1 !< Init grib1 @return N/A
+     procedure :: init_grib2 !< Init grib2 @return N/A
+     procedure :: gdswzd => GDSWZD_station_points !< gsdwzd() @return N/A
   end type ip_station_points_grid
 
 contains
 
+  !> Init GRIB1.
+  !>
+  !> @param[inout] self ???
+  !> @param[in] g1_desc ???
+  !>
+  !> @author Iredell @date 96-04-10
   subroutine init_grib1(self, g1_desc)
     class(ip_station_points_grid), intent(inout) :: self
     type(grib1_descriptor), intent(in) :: g1_desc
-
-    
   end subroutine init_grib1
 
+  !> Init GRIB2.
+  !>
+  !> @param[inout] self ???
+  !> @param[in] g2_desc ???
+  !>
+  !> @author Iredell @date 96-04-10
   subroutine init_grib2(self, g2_desc)
     class(ip_station_points_grid), intent(inout) :: self
     type(grib2_descriptor), intent(in) :: g2_desc
   end subroutine init_grib2
 
-   SUBROUTINE GDSWZD_station_points(self,IOPT,NPTS, &
+  !> ???
+  !>
+  !> @param[in] self ???
+  !> @param[in] IOPT ???
+  !> @param[in] NPTS ???
+  !> @param[in] FILL ???
+  !> @param[inout] XPTS ???
+  !> @param[inout] YPTS ???
+  !> @param[inout] RLON ???
+  !> @param[inout] RLAT ???
+  !> @param[out] NRET ???
+  !> @param[out] CROT ???
+  !> @param[out] SROT ???
+  !> @param[out] XLON ???
+  !> @param[out] XLAT ???
+  !> @param[out] YLON ???
+  !> @param[out] YLAT ???
+  !> @param[out] AREA ???
+  !>
+  !> @author Iredell @date 96-04-10
+  SUBROUTINE GDSWZD_station_points(self,IOPT,NPTS, &
        FILL,XPTS,YPTS,RLON,RLAT,NRET, &
        CROT,SROT,XLON,XLAT,YLON,YLAT,AREA)
     class(ip_station_points_grid), intent(in) :: self
@@ -41,10 +79,7 @@ contains
     REAL,  OPTIONAL,         INTENT(  OUT) :: CROT(NPTS),SROT(NPTS)
     REAL,  OPTIONAL,         INTENT(  OUT) :: XLON(NPTS),XLAT(NPTS)
     REAL,  OPTIONAL,         INTENT(  OUT) :: YLON(NPTS),YLAT(NPTS),AREA(NPTS)
-    
+
   end subroutine GDSWZD_STATION_POINTS
-  
+
 end module ip_station_points_grid_mod
-
-
-  
