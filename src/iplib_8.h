2af1f7370582d205dc1f91d57490c153e1fcfc0d
Edward Hartnett <edward.hartnett@noaa.gov>
2023-03-07 05:59:43 -0700

    added _8 include file again

---
+++ src/iplib_8.h
@@ -0,0 +1,15 @@
+/** @file
+ * @brief C interface to gdswzd() function for '4' library build.
+ * @author NOAA Programmer 
+ */
+
+#ifndef IPLIB
+#define IPLIB
+
+void gdswzd(long *kgds, long iopt, long npts, double fill,
+            double *xpts, double *ypts, double *rlon, double *rlat,
+            long *nret,
+            double *crot, double *srot, double *xlon, double *xlat,
+            double *ylon, double *ylat, double *area);
+
+#endif
