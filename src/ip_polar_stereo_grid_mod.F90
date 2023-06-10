4c0968274a4f7579c7ce99d34338054e51785320
Alex Richert <alexander.richert@noaa.gov>
2023-06-09 17:49:31 -0700

    fully remove unused _ERROR subroutines

--- src/ip_polar_stereo_grid_mod.F90
+++ src/ip_polar_stereo_grid_mod.F90
@@ -459,43 +459,6 @@ CONTAINS
     ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   END SUBROUTINE GDSWZD_POLAR_STEREO
 
-  !> Error handler.
-  !>
-  !> Upon an error, this subprogram assigns a fill value to the output
-  !> fields.
-  !>
-  !> @param[in] iopt option flag
-  !> - 1 to compute earth coords of selected grid coords
-  !> - -1 to compute grid coords of selected earth coords
-  !> @param[in] fill fill value to set invalid output data
-  !> (must be impossible value; suggested value: -9999.)
-  !> @param[out] rlat (npts) earth latitudes in degrees n if iopt<0
-  !> @param[out] rlon (npts) earth longitudes in degrees e if iopt<0
-  !> @param[out] xpts (npts) grid x point coordinates if iopt>0
-  !> @param[out] ypts (npts) grid y point coordinates if iopt>0
-  !> @param[in] npts maximum number of coordinates
-  !>
-  !> @author Gayno @date 2015-07-13
-!  SUBROUTINE POLAR_STEREO_ERROR(IOPT,FILL,RLAT,RLON,XPTS,YPTS,NPTS)
-!    IMPLICIT NONE
-!    !
-!    INTEGER, INTENT(IN   ) :: IOPT, NPTS
-!    !
-!    REAL,    INTENT(IN   ) :: FILL
-!    REAL,    INTENT(  OUT) :: RLAT(NPTS),RLON(NPTS)
-!    REAL,    INTENT(  OUT) :: XPTS(NPTS),YPTS(NPTS)
-!    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-!    IF(IOPT>=0) THEN
-!       RLON=FILL
-!       RLAT=FILL
-!    ENDIF
-!    IF(IOPT<=0) THEN
-!       XPTS=FILL
-!       YPTS=FILL
-!    ENDIF
-!    ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-!  END SUBROUTINE POLAR_STEREO_ERROR
-
   !> Vector rotation fields for polar stereographic grids.
   !>
   !>
