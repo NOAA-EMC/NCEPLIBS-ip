0d71b121309ab27d1458262051907e45eb732d45
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2026-08-12 09:08:12 -0400

    Fix allocation logic in tests/test_ip_version.F90.

--- tests/test_ip_version.F90
+++ tests/test_ip_version.F90
@@ -2,8 +2,8 @@ program test_ip_version
    use ip_mod, only: ip_version
    implicit none
 
+   character(len=256) :: ctemp
    character(len=:), allocatable :: expected_version
-   integer :: argument_length
    integer :: status
 
    if (command_argument_count() /= 1) then
@@ -11,19 +11,13 @@ program test_ip_version
       error stop 1
    endif
 
-   call get_command_argument(1, length=argument_length, status=status)
+   call get_command_argument(1, ctemp, status=status)
    if (status /= 0) then
       write(*, '(a)') "Unable to determine command-line argument length"
       error stop 1
    endif
 
-   allocate(character(len=argument_length) :: expected_version)
-
-   call get_command_argument(1, value=expected_version, status=status)
-   if (status /= 0) then
-      write(*, '(a)') "Unable to read expected version"
-      error stop 1
-   endif
+   expected_version=trim(ctemp)
 
    if (expected_version /= ip_version) then
       write(*, '(a)') "Version mismatch"
