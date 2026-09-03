930bdb97265c9fc6057d2a3e91d043745336e7ba
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-09-03 08:05:01 -0700

    add placeholders for batch of unit tests

---
+++ tests/test_sptgps.F90
@@ -0,0 +1,11 @@
+! This is a test for NCEPLIBS-ip library.
+!
+! This tests the sptgps() subroutine.
+! 
+! Alyson Stahl 9/2026
+program test_sptgps
+  use sp_mod
+  implicit none
+
+  print *, "Success!"
+end program test_sptgps
